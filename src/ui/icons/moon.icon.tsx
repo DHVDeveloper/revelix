65498294855dd05abd52acd63918eee638c4fc81
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-12 19:46:10 +0200

    chore: added icons for theme toggle.

---
+++ src/ui/icons/moon.icon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function MoonIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      className={`${props.className || ""}`}
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9 9 0 0 1 12 21"
+      ></path>
+    </svg>
+  )
+}
