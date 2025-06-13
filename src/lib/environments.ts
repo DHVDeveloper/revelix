8fd1f34eb54a9de61d8c23593f8701cbe5ea41d8
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-13 20:13:56 +0200

    chore: added environment constant.

---
+++ src/lib/environments.ts
@@ -0,0 +1,4 @@
+export const AppConfig = {
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://autogest-backend-5p1l.onrender.com',
+  COOKIE_NAME: process.env.NEXT_PUBLIC_COOKIE_NAME || 'sessionToken',
+}
