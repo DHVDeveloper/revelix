85be46d1501b2952827951e57c82a0db7764171c
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-14 14:04:29 +0200

    chore: added alert component.

---
+++ src/views/shared/components/alert/atert.tsx
@@ -0,0 +1,27 @@
+import { AlertData } from "@/context/alert/alert.interface"
+import styles from "./alert.module.css"
+
+export function Alert({
+  alert,
+  visible,
+  onClose,
+}: {
+  alert: AlertData
+  visible: boolean
+  onClose: () => void
+}) {
+  return (
+    <div
+      className={`${styles.container} ${
+        visible ? "animate-fade-in" : "animate-fade-out"
+      }`}
+    >
+      <div className={`${styles.alert} ${styles[alert.type]}`}>
+        <span className={styles.message}>{alert.message}</span>
+        <button onClick={onClose} className={styles.closeBtn}>
+          X
+        </button>
+      </div>
+    </div>
+  )
+}
