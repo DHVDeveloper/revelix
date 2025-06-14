e0779abca08354ce975a330b6058d8dadcbedf3f
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-14 15:45:05 +0200

    chore: fix auth service.

--- src/application/service/auth.service.ts
+++ src/application/service/auth.service.ts
@@ -1,7 +1,5 @@
 import { AuthServices } from "@/domain/port/auth-service.port"
 import { login, logout } from "../../infraestructure/repository/auth.repository"
-import { setCookie } from "@/utils/cookie"
-import { AppConfig } from "@/lib/environments"
 
 export const authServices: AuthServices = ({
   login: async (email, password) => {
@@ -11,10 +9,6 @@ export const authServices: AuthServices = ({
       return {success: false, error: response.error}
     }
 
-    if (response.data) {
-      setCookie(AppConfig.COOKIE_NAME, response.data.token)
-    }
-   
     return {success:true}
   },
   
