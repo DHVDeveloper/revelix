ca518f3b31b643e8897ab917ff984ce35aab765a
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-14 16:11:21 +0200

    feat: added login form.

---
+++ src/views/pages/login/login-form/login-form.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { authServices } from "@/application/service/auth.service"
+import { useAlertContext } from "@/views/shared/context/alert/alert.context"
+import { useState } from "react"
+import styles from "./login-form.module.css"
+
+
+export function LoginForm() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const {showAlert} = useAlertContext()
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    setIsLoading(true)
+    e.preventDefault()
+    
+    const authResponse = await authServices.login(email,password)
+
+    if(!authResponse.success) {
+      showAlert({message: authResponse.error ?? 'Login error', type:'danger'})
+      setIsLoading(false)
+      return
+    }
+
+    showAlert({message: 'You have logged in successfully!', type: 'success'})
+    setIsLoading(false)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.mainContainer}>
+      <input className={styles.input} required onChange={(e) => setEmail(e.target.value)} placeholder="Username" type="email" />
+      <input className={styles.input} required onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
+      <button disabled={isLoading} className={`${styles.button} ${isLoading ? 'animate-pulse' : ''}`}>{isLoading ? 'Loading...' : 'Sign in'}</button>
+    </form>
+  )
+}
