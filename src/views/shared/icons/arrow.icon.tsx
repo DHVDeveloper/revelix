926e1add5848aca9db17ca3361684dd46dc134ff
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-15 23:49:33 +0200

    chore: added arrow icon.

---
+++ src/views/shared/icons/arrow.icon.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function ArrowIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" className={`${props.className || ""}`} {...props}>
+      <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
+    </svg>
+  )
+}
