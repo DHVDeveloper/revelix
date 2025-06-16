ab5098cd8fc6bdf519c05760e930be01d51bd75a
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-16 18:52:46 +0200

    feat: added movies section on home page.

--- src/views/pages/home/home.page.tsx
+++ src/views/pages/home/home.page.tsx
@@ -2,17 +2,25 @@
 import { Movie } from "@/domain/entities/movie.entity";
 import { HomeHeader } from "./sections/header/home-header";
 import { HomeGenreMovies } from "./sections/movies/genre-movies/home-genre-movies";
+import { HomeSoonMovies } from "./sections/movies/soon-movies/home-soon-movies";
+import styles from "./home.page.module.css"
+import { Genre } from "@/domain/entities/genre.entity";
+import { HomeUserMovies } from "./sections/movies/user-movies/home-user-movies";
 
 interface HomePageProps {
   allMovies:Movie[]
+  genreList: Genre[]
 }
-export function HomePage({allMovies}:HomePageProps) {
+export function HomePage({allMovies, genreList}:HomePageProps) {
 
   return (
-    <div>
+    <main>
       <HomeHeader movieList={allMovies.slice(0,5)} />
-      <HomeGenreMovies />
-
-    </div>
+      <div className={styles.main}>
+        <HomeGenreMovies genreList={genreList} />
+        <HomeSoonMovies movieList={allMovies}/>
+        <HomeUserMovies genreList={genreList} />
+      </div>
+    </main>
   )
 }
