107f87fdf8e895dc4a2e7605f50d82eae8ba35b7
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-17 22:12:29 +0200

    fix: auth service.

--- src/infraestructure/repository/auth.repository.ts
+++ src/infraestructure/repository/auth.repository.ts
@@ -1,19 +1,17 @@
+"use server"
 import { AppConfig } from "@/lib/environments"
-import { AuthRequestBody, LoginResponse } from "../interfaces/auth-response"
 import { ApiResponse } from "@/types/api-response.interface"
+import { apiFetch } from "@/utils/fetch"
+import { cookies } from "next/headers"
+import { AuthRequestBody } from "../interfaces/auth-response"
 
-export async function login(credentials: AuthRequestBody): Promise<ApiResponse<LoginResponse>> {
+export async function login(credentials: AuthRequestBody): Promise<ApiResponse<void>> {
   try {
-    const response = await fetch(`/api/auth/login`, {
+    const response = await apiFetch(`${AppConfig.API_BASE_URL}/films/auth/sign-in`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
       body: JSON.stringify(credentials),
     })
 
-    const data = await response.json()
     if (!response.ok) {
       if(response.status === 500) {
         return {
@@ -26,10 +24,22 @@ export async function login(credentials: AuthRequestBody): Promise<ApiResponse<L
         error: "User not found",
       }
     }
+    
+    const { token } = await response.json();
+    const clientCookies = await cookies();
+    
+    clientCookies.set({
+      name: AppConfig.COOKIE_NAME,
+      value: token,
+      httpOnly: false,
+      secure: AppConfig.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
 
     return {
       success: true,
-      data,
     }
 
   } catch {
@@ -42,17 +52,14 @@ export async function login(credentials: AuthRequestBody): Promise<ApiResponse<L
 
 export async function logout(): Promise<ApiResponse<void>> {
   try {
-    const response = await fetch(`${AppConfig.API_BASE_URL}/films/auth/logout`, {
-      method: "POST",
-      credentials: "include",
-    })
+    const response = await apiFetch(`${AppConfig.API_BASE_URL}/films/auth/sign-out`)
 
     const data = await response.json()
 
     if (!response.ok) {
       return {
         success: false,
-        error: data?.message || "Error logging out",
+        error: data?.message || "Error logout",
       }
     }
 
