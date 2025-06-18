ccbf4677abcc49f57854e68e545c57e5de2af475
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-18 23:43:57 +0200

    fix: build.

--- src/app/(user-layout)/movies/[movieSlug]/page.tsx
+++ src/app/(user-layout)/movies/[movieSlug]/page.tsx
@@ -2,7 +2,12 @@ import { genreServices } from "@/application/service/genre.service"
 import { movieServices } from "@/application/service/movie.service"
 import { MovieDetailsPage } from "@/views/pages/movie-details/movie-details.page"
 
-export default async function MovieDetails({ params }: { params: { movieSlug: string } }) {
+export default async function MovieDetails({
+  params,
+}: {
+  params: Promise<{ movieSlug: string }>
+}) {
+  const { movieSlug } = await params
   const [genresRes, userMoviesRes] = await Promise.all([
     genreServices.genres(),
     movieServices.userMovies(),
@@ -13,7 +18,7 @@ export default async function MovieDetails({ params }: { params: { movieSlug: st
 
   return (
     <MovieDetailsPage
-      slug={params.movieSlug}
+      slug={movieSlug}
       genreList={genreList}
       userMovieIds={userMovieIds}
     />
