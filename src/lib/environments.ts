7bf2f57516abafa3d5ade311b208705d42eef24e
Daniel Vieira <147045500+DHVDeveloper@users.noreply.github.com>
2025-06-20 08:55:38 +0100

    Update environments.ts

--- src/lib/environments.ts
+++ src/lib/environments.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://autogest-backend-5p1l.onrender.com',
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://kata.conducerevel.com',
   COOKIE_NAME: process.env.NEXT_PUBLIC_COOKIE_NAME || 'sessionToken',
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV || 'production'
 }
