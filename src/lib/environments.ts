ea5cb3cf9361a64513bce56b3f4a2328959f7525
Daniel Vieira <147045500+DHVDeveloper@users.noreply.github.com>
2025-06-20 09:25:55 +0100

    Update environments.ts

--- src/lib/environments.ts
+++ src/lib/environments.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
   API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://kata.conducerevel.com',
-  COOKIE_NAME: process.env.NEXT_PUBLIC_COOKIE_NAME || 'sessionToken',
+  COOKIE_NAME: process.env.NEXT_PUBLIC_COOKIE_NAME || 'access_token',
   NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV || 'production'
 }
